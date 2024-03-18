6a9549d46410f14ad978a796216c076484370d7b
kaikaci12 <somewonesomewone@gmail.com>
2024-03-19 00:50:25 +0400

    get close tag, and on click, it resets all main and header styles

--- app.js
+++ app.js
@@ -5,3 +5,17 @@ button.addEventListener("click", () => {
 const burgerIcon = document.querySelector(".burger-meniu");
 const navigation = document.querySelector("nav");
 const W = document.querySelector(".W-icon");
+
+burgerIcon.addEventListener("click", () => {
+  navigation.classList.toggle("navActive");
+  W.style.opacity = "0.5";
+  main.style.opacity = "0.5";
+  navigation.style.opacity = "1 !important";
+});
+const close = document.querySelector(".close");
+
+close.addEventListener("click", () => {
+  navigation.classList.toggle("navActive");
+  W.style.opacity = "1";
+  main.style.opacity = "1";
+});
