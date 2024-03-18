0b5913990342953ffcc02e089f72a717c427d895
kaikaci12 <somewonesomewone@gmail.com>
2024-03-19 00:18:56 +0400

    get burger-icon nav and W-icon with queryselection

--- app.js
+++ app.js
@@ -2,3 +2,6 @@ const button = document.querySelector(".read-more");
 button.addEventListener("click", () => {
   document.location.href = "#end";
 });
+const burgerIcon = document.querySelector(".burger-meniu");
+const navigation = document.querySelector("nav");
+const W = document.querySelector(".W-icon");
