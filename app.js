e5c93658343b5a13f05825f3fdb996983271d4bb
kaikaci12 <somewonesomewone@gmail.com>
2024-03-19 00:17:54 +0400

    add event listener on button

--- app.js
+++ app.js
@@ -1,21 +1,4 @@
 const button = document.querySelector(".read-more");
 button.addEventListener("click", () => {
-  location.reload();
-});
-const burgerIcon = document.querySelector(".burger-meniu");
-const navigation = document.querySelector("nav");
-const W = document.querySelector(".W-icon");
-
-burgerIcon.addEventListener("click", () => {
-  navigation.classList.toggle("navActive");
-  W.style.opacity = "0.5";
-  main.style.opacity = "0.5";
-  navigation.style.opacity = "1 !important";
-});
-const close = document.querySelector(".close");
-
-close.addEventListener("click", () => {
-  navigation.classList.toggle("navActive");
-  W.style.opacity = "1";
-  main.style.opacity = "1";
+  document.location.href = "#end";
 });
